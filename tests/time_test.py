f81d46ee9eb5fb81c70c447f2a69d2d8fac5ff3f
Albert Liu <albertymliu@gmail.com>
2018-10-02 21:10:12 -0400

    time_test is ready, but cython implementations aren't yet

--- tests/time_test.py
+++ tests/time_test.py
@@ -26,9 +26,17 @@ def timeTest():
 def timeTest0():
 	global SEQ1, SEQ2, GAP, MATCH, MISMATCH
 	t0 = time.time()
-	global_align0(SEQ1, LEN1, SEQ2, LEN2, GAP, MATCH, MISMATCH)
-	semiglobal_align0(SEQ1, LEN1, SEQ2, LEN2, GAP, MATCH, MISMATCH)
-	local_align0(SEQ1, LEN1, SEQ2, LEN2, GAP, MATCH, MISMATCH)
+	cseq1 = encodeDNA(SEQ1)
+	cseq2 = encodeDNA(SEQ2)
+	out,align1,align2 = global_align0(cseq1, LEN1, cseq2, LEN2, GAP, MATCH, MISMATCH)
+	align1 = decodeDNA(align1)
+	align2 = decodeDNA(align2)
+	out,align1,align2 = semiglobal_align0(cseq1, LEN1, cseq2, LEN2, GAP, MATCH, MISMATCH)
+	align1 = decodeDNA(align1)
+	align2 = decodeDNA(align2)
+	out,align1,align2 = local_align0(cseq1, LEN1, cseq2, LEN2, GAP, MATCH, MISMATCH)
+	align1 = decodeDNA(align1)
+	align2 = decodeDNA(align2)
 	return time.time()-t0
 
 def timeData(runs):
@@ -42,6 +50,16 @@ def timeData(runs):
 		min_t = min(min_t,current)
 	return total/runs,max_t,min_t
 
+def timeData0(runs):
+	total=timeTest0()
+	min_t = total
+	max_t = total
+	for _ in range(1,runs):
+		current = timeTest0()
+		total+=current
+		max_t = max(max_t, current)
+		min_t = min(min_t,current)
+	return total/runs,max_t,min_t
 
-avg_time,max_time,min_time = timeData(RUNS)
+avg_time,max_time,min_time = timeData0(RUNS)
 print("Data over {} runs:\n\tAVG:\t{}\n\tMAX:\t{}\n\tMIN:\t{}\n".format(RUNS,avg_time,max_time,min_time))
